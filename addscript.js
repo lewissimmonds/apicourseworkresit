fffad221fb488eb0257884c88f97aca42cad6767
Lewis Simmonds <simmondslewis5@gmail.com>
2022-07-16 17:21:19 +0100

    listed languages now formatted correctly

---
+++ addscript.js
@@ -0,0 +1,20 @@
+const countries =
+[
+
+{
+  "name" : "ghana",
+  "code" : "GH",
+  "number" : 1,
+},
+{
+  "name" : "united kingdom",
+  "code" : "GB",
+  "number" : 2,
+},
+{
+  "name" : "norway",
+  "code" : "NO",
+  "number" : 3,
+},
+
+]
